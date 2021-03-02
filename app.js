47c6b65fc64b25caf64247df40c815a8dad78883
Leily <leilymrz@gmail.com>
2021-03-01 17:46:30 -0800

    practice manipulating events

--- app.js
+++ app.js
@@ -1,10 +1,75 @@
+// const display1 = document.querySelector('#display1');
+// const display2 = document.querySelector('#display2');
+
+// const player1 = document.querySelector('#player1');
+// const player2 = document.querySelector('#player2');
+// const buttonR = document.querySelector('#buttonR');
+
+// const setWinningSc = document.querySelector('#setWinningSc');
+
+// let score1 = 0;
+// let score2 = 0;
+// let winningSc = 3;
+// let gameOver = false;
+
+// setWinningSc.addEventListener('change', function () {
+//     winningSc = parseInt(this.value);
+//     reset();
+// })
+
+// player1.addEventListener('click', function () {
+//     // console.log('player1')
+//     if (!gameOver) {
+//         score1 += 1;
+//         if (score1 === winningSc) {
+//             gameOver = true;
+//             display1.classList.add('has-text-success');
+//             display2.classList.add('has-text-danger');
+//             player1.disabled = true
+//             player2.disabled = true
+//         }
+//         display1.textContent = score1;
+//     }
+// })
+
+// player2.addEventListener('click', function () {
+//     // console.log('player2')
+//     if (!gameOver) {
+//         score2 += 1;
+//         if (score2 === winningSc) {
+//             gameOver = true;
+//             display2.classList.add('has-text-success');
+//             display1.classList.add('has-text-danger');
+//             player1.disabled = true
+//             player2.disabled = true
+//         }
+//         display2.textContent = score2;
+//     }
+// })
+
+// buttonR.addEventListener('click', reset);
+
+// function reset() {
+//     gameOver = false;
+//     score1 = 0;
+//     score2 = 0;
+//     display1.textContent = 0;
+//     display2.textContent = 0;
+//     display1.classList.remove('has-text-success', 'has-text-danger');
+//     display2.classList.remove('has-text-success', 'has-text-danger');
+//     player1.disabled = false
+//     player2.disabled = false
+// }
+
+//REFACTORING
+
 const display1 = document.querySelector('#display1');
 const display2 = document.querySelector('#display2');
-// const input = document.querySelector('#playing');
 
 const player1 = document.querySelector('#player1');
 const player2 = document.querySelector('#player2');
 const buttonR = document.querySelector('#buttonR');
+
 const setWinningSc = document.querySelector('#setWinningSc');
 
 let score1 = 0;
@@ -23,8 +88,10 @@ player1.addEventListener('click', function () {
         score1 += 1;
         if (score1 === winningSc) {
             gameOver = true;
-            display1.classList.add('winner');
-            display2.classList.add('loser');
+            display1.classList.add('has-text-success');
+            display2.classList.add('has-text-danger');
+            player1.disabled = true
+            player2.disabled = true
         }
         display1.textContent = score1;
     }
@@ -36,8 +103,10 @@ player2.addEventListener('click', function () {
         score2 += 1;
         if (score2 === winningSc) {
             gameOver = true;
-            display2.classList.add('winner');
-            display1.classList.add('loser');
+            display2.classList.add('has-text-success');
+            display1.classList.add('has-text-danger');
+            player1.disabled = true
+            player2.disabled = true
         }
         display2.textContent = score2;
     }
@@ -51,6 +120,8 @@ function reset() {
     score2 = 0;
     display1.textContent = 0;
     display2.textContent = 0;
-    display1.classList.remove('winner', 'loser');
-    display2.classList.remove('winner', 'loser');
+    display1.classList.remove('has-text-success', 'has-text-danger');
+    display2.classList.remove('has-text-success', 'has-text-danger');
+    player1.disabled = false
+    player2.disabled = false
 }
